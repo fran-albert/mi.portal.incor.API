ca3f1837449eed0fe70ca300731e1bffdb9f6c68
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2023-12-14 21:34:07 -0300

    Add validation rules and properties to CreateUserDto class

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,46 @@
-export class CreateUserDto {}
+import { Transform } from 'class-transformer';
+import {
+  IsDate,
+  IsDateString,
+  IsEmail,
+  IsInt,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+export class CreateUserDto {
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+  @IsString()
+  lastname: string;
+
+  @IsString()
+  dni: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  password: string;
+
+  @IsDateString()
+  birthDate: Date;
+
+  @IsString()
+  phone: string;
+
+  @IsInt()
+  @IsPositive()
+  idCity: number;
+
+  @IsString()
+  @IsOptional()
+  photo?: string;
+
+  @IsString()
+  healthInsurance: string;
+}
