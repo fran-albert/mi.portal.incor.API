c53081c8ddb2362a48d6c39fb9651096e96c23a7
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2023-12-15 14:45:23 -0300

    Add DataLab entity and create OneToOne relationship with Lab entity

--- src/labs/entities/lab.entity.ts
+++ src/labs/entities/lab.entity.ts
@@ -1,3 +1,4 @@
+import { DataLab } from 'src/data-labs/entities/data-lab.entity';
 import { User } from 'src/users/entities/user.entity';
 import {
   Column,
@@ -5,6 +6,7 @@ import {
   Entity,
   JoinColumn,
   ManyToOne,
+  OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
@@ -14,10 +16,12 @@ export class Lab {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @ManyToOne(() => User, (user) => user.labs)
+  @ManyToOne(() => User, (user) => user.labs, { nullable: false })
   @JoinColumn({ name: 'idPatient' })
   user: User;
-  
+
+  @OneToOne(() => DataLab, (datalab) => datalab.lab)
+  datalab: DataLab;
 
   @Column()
   name: string;
