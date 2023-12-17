9ea26a671560b33f2ba45809740c1fcc04a94155
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2023-12-17 00:17:34 -0300

    Refactor DataLabsService with dependency injection and async/await

--- src/data-labs/data-labs.service.ts
+++ src/data-labs/data-labs.service.ts
@@ -1,26 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateDataLabDto } from './dto/create-data-lab.dto';
 import { UpdateDataLabDto } from './dto/update-data-lab.dto';
+import { DataLab } from './entities/data-lab.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { LabsService } from 'src/labs/labs.service';
 
 @Injectable()
 export class DataLabsService {
-  create(createDataLabDto: CreateDataLabDto) {
-    return 'This action adds a new dataLab';
+  constructor(
+    @InjectRepository(DataLab)
+    private readonly dataLabRepository: Repository<DataLab>,
+    private readonly labService: LabsService,
+  ) {}
+
+  async create(createDataLabDto: CreateDataLabDto) {
+    const lab = await this.labService.findOne(createDataLabDto.idLab);
+
+    const newDataLab = {
+      ...createDataLabDto,
+      lab: lab,
+    };
+
+    return this.dataLabRepository.save(newDataLab);
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all dataLabs`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} dataLab`;
+  async findOne(id: number) {
+    const dataLab = await this.dataLabRepository.findOneBy({ id });
+
+    if (!dataLab) {
+      throw new BadRequestException('DataLab not found');
+    }
+
+    return dataLab;
   }
 
-  update(id: number, updateDataLabDto: UpdateDataLabDto) {
+  async update(id: number, updateDataLabDto: UpdateDataLabDto) {
     return `This action updates a #${id} dataLab`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} dataLab`;
   }
 }
