1226905a482b99a4445fc7562c621808e210401b
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2023-12-17 00:17:52 -0300

    Add LabsModule import to DataLabsModule

--- src/data-labs/data-labs.module.ts
+++ src/data-labs/data-labs.module.ts
@@ -3,9 +3,10 @@ import { DataLabsService } from './data-labs.service';
 import { DataLabsController } from './data-labs.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataLab } from './entities/data-lab.entity';
+import { LabsModule } from 'src/labs/labs.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DataLab])],
+  imports: [TypeOrmModule.forFeature([DataLab]), LabsModule],
   controllers: [DataLabsController],
   providers: [DataLabsService],
   exports: [DataLabsService],
