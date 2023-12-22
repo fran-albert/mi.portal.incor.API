005b9f04d984ffae4bb922209f4fb8bb948b1b94
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2023-12-22 00:13:53 -0300

    Add AuthGuard to protect profile route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
@@ -7,12 +7,13 @@ import { Auth } from './decorators/auth.decorator';
 import { UserActiveInterface } from '../common/interface/user-active.interface';
 import { ActiveUser } from '../common/decorators/active-user.decorator';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { AuthGuard } from './guard/auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  
-//   @Auth(Role.ADMINISTRADOR)
+
+  //   @Auth(Role.ADMINISTRADOR)
   @Post('register')
   register(
     @Body()
@@ -31,8 +32,8 @@ export class AuthController {
 
   @ApiBearerAuth()
   @Get('profile')
-  @Auth(Role.PACIENTE)
+  @UseGuards(AuthGuard)
   getProfile(@ActiveUser() user: UserActiveInterface) {
-    return this.authService.getProfile(user);
+    return this.authService.getProfile(user.id);
   }
 }
