3dc96388d8741649291a0052595c67e1a718eb57
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2023-12-24 02:41:52 -0300

    Add UploadModule to LabsModule imports

--- src/labs/labs.module.ts
+++ src/labs/labs.module.ts
@@ -4,9 +4,10 @@ import { LabsController } from './labs.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lab } from './entities/lab.entity';
 import { UsersModule } from 'src/users/users.module';
+import { UploadModule } from 'src/upload/upload.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Lab]), UsersModule],
+  imports: [TypeOrmModule.forFeature([Lab]), UsersModule, UploadModule],
   controllers: [LabsController],
   providers: [LabsService],
   exports: [LabsService],
