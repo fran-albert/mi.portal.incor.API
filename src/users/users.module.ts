969f0bba661070c4ca7499ca808ee7921788094d
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-01-04 20:31:27 -0300

    Add CitiesModule to users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UploadModule } from 'src/upload/upload.module';
+import { CitiesModule } from 'src/cities/cities.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), UploadModule],
+  imports: [TypeOrmModule.forFeature([User]), UploadModule, CitiesModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService]
