c559bd71b6985bf2205aff63ab89deb999596fc2
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-01-05 12:00:02 -0300

    Add AuthModule to DataLabsModule imports

--- src/data-labs/data-labs.module.ts
+++ src/data-labs/data-labs.module.ts
@@ -4,9 +4,10 @@ import { DataLabsController } from './data-labs.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataLab } from './entities/data-lab.entity';
 import { LabsModule } from 'src/labs/labs.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DataLab]), LabsModule],
+  imports: [TypeOrmModule.forFeature([DataLab]), LabsModule, AuthModule],
   controllers: [DataLabsController],
   providers: [DataLabsService],
   exports: [DataLabsService],
