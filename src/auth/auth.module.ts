3974ce5afc35400cd2aff0590bc85863550ed5e3
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-01-05 11:59:57 -0300

    Refactor JWT configuration in AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,27 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { UsersModule } from "src/users/users.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { JwtModule } from "@nestjs/jwt";
-import { jwtConstants } from "./constants/jwt.constant";
 import { EmailModule } from "src/email/email.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
-    UsersModule,
+    forwardRef(() => (UsersModule)),
     EmailModule,
-    JwtModule.register({
-      global: true, // cualquier servicio  puede utulizar JWT
-      secret: jwtConstants.secret, // el secret es la palabra secreta que se ocupa el JWT para verificar que el token se generó es correcto
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        global: true,
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
+  exports:[JwtModule]
 })
 export class AuthModule {}
