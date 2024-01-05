73356065362d88c0d725ce95294ad2b6382377ce
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-01-05 12:00:07 -0300

    Add AuthModule to UsersModule imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UploadModule } from 'src/upload/upload.module';
 import { CitiesModule } from 'src/cities/cities.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), UploadModule, CitiesModule],
+  imports: [TypeOrmModule.forFeature([User]), UploadModule, CitiesModule, forwardRef(() => AuthModule)],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService]
